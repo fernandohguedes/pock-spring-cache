5c8fb9a91c0255edbeafc19b0d12473121c21959
Fernando Haubert Guedes <fernando.guedes@cwi.com.br>
2025-08-26 22:38:23 -0300

    feat: adicionando anotação de cache

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -2,8 +2,8 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Product;
 import com.example.demo.repository.ProductRepository;
-import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +12,7 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Cacheable("produtos")
     public Product getProduct(Long id){
         System.out.println("Buscando do banco de dados...");
         return repository.findById(id).orElse(null);
